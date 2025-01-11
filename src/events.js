66c73b3e0f0811335d9beb76a00fb65267306066
divyeshswiftrut <divyesh.variya@swiftrut.com>
2025-01-12 00:57:31 +0530

    12/1/25

--- src/events.js
+++ src/events.js
@@ -3,7 +3,7 @@ function loadMoreItems() {
   const hiddenItems = document.querySelectorAll(".grid-item.hidden");
 
   // Show up to 4 hidden items
-  for (let i = 0; i < 3 && i < hiddenItems.length; i++) {
+  for (let i = 0; i < 2 && i < hiddenItems.length; i++) {
     hiddenItems[i].classList.remove("hidden");
   }
 
